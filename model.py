5de440c5ee5b77ce4b226ea74a345635bfc06552
leolopes <leo.lopes@epita.fr>
2024-06-10 15:56:27 +0200

    working, but model very bad

--- model.py
+++ model.py
@@ -33,59 +33,65 @@ class CoRDataset(Dataset):
     def __getitem__(self, idx):
         img_path, class_name = self.data[idx]
         class_id = self.class_map[class_name]
-        img_tensor = torch.from_numpy(self.img[idx])
-        class_id = torch.tensor([class_id])
-        return img_tensor.float(), class_id.float()
+        img_tensor = torch.from_numpy(self.img[idx]).unsqueeze(0)  # Add channel dimension
+        return img_tensor.float(), class_id  # Return scalar class_id
     
 
 def Print_loss_accuracy(nepoch, tloss, vloss, accuracy, best_tloss, best_vloss, best_accuracy):
     print ("{:<6} {:<15} {:<17} {:<15} {:<20} {:<22} {:<15}".format(nepoch, tloss, vloss, accuracy, best_tloss, best_vloss, best_accuracy))
 
 def Learning(nepoch, model, crit, optim, batchsize, trainingloader, validationloader):
-    #writer = SummaryWriter(optim)
     best_tloss = 100.
     best_vloss = 100.
     best_accuracy = 0.
-    Print_loss_accuracy('Epoch', 'training loss', 'validation loss', 'accuracy', 'best train loss', 'best validation loss', 'best accuracy')
+    Print_loss_accuracy('Epoch', 'training loss', 'validation loss', 'accuracy', 'best train loss',
+                        'best validation loss', 'best accuracy')
+
     for nepoch in range(nepoch):
         tloss = 0.
         vloss = 0.
         correct_test = 0
         model.train()
+
         for images, labels in trainingloader:
-            optim.zero_grad() 
-            predicted = model(images.view(1, -1))
-            loss = crit(predicted.squeeze(), labels.squeeze())
+            optim.zero_grad()
+            images = images.view(images.size(0), -1)  # Flatten the images
+            predicted = model(images)
+            loss = crit(predicted, labels)  # Ensure labels are not squeezed
             loss.backward()
             optim.step()
             tloss += loss.item() * images.size(0)
-        tloss /= len(trainingloader.sampler)
+
+        tloss /= len(trainingloader.dataset)
+
         model.eval()
-        for images, labels in validationloader:
-            predicted = model(images.view(1,-1)) 
-            loss = crit(predicted.squeeze(), labels.squeeze())
-            correct_test += np.sum(torch.squeeze(predicted).round().detach().numpy() == labels.squeeze().detach().numpy())
-            vloss += loss.item() * images.size(0)
-        vloss /= len(validationloader.sampler)
-        accuracy = 100 * correct_test/len(validationloader)
-        #writer.add_scalar('Accuracy', accuracy, nepoch)
-        #writer.add_scalar('Loss/test', tloss, nepoch)
-        #writer.add_scalar('Loss/validation', vloss, nepoch)
+
+        with torch.no_grad():
+            for images, labels in validationloader:
+                images = images.view(images.size(0), -1)  # Flatten the images
+                predicted = model(images)
+                loss = crit(predicted, labels)
+                correct_test += (predicted.argmax(1) == labels).sum().item()
+                vloss += loss.item() * images.size(0)
+
+        vloss /= len(validationloader.dataset)
+        accuracy = 100 * correct_test / len(validationloader.dataset)
 
         if accuracy >= best_accuracy:
             torch.save(model, "best_model.pth")
-            best_accuracy=accuracy
+            best_accuracy = accuracy
         if vloss <= best_vloss:
             best_vloss = vloss
         if tloss <= best_tloss:
             best_tloss = tloss
-        Print_loss_accuracy(nepoch+1, 
-                            np.round(tloss,8), 
-                            np.round(vloss,8), 
-                            np.round(accuracy,8), 
-                            np.round(best_tloss,8), 
-                            np.round(best_vloss,8), 
-                            np.round(best_accuracy,8))
+
+        Print_loss_accuracy(nepoch + 1,
+                            np.round(tloss, 8),
+                            np.round(vloss, 8),
+                            np.round(accuracy, 8),
+                            np.round(best_tloss, 8),
+                            np.round(best_vloss, 8),
+                            np.round(best_accuracy, 8))
         
 def Testmodel(modelfile,crit, testloader):
     model = torch.load(modelfile)
@@ -104,25 +110,24 @@ def Testmodel(modelfile,crit, testloader):
                                                                        torch.squeeze(predicted).round().detach().numpy(),
                                                                                     np.round(test_loss.item(), 2)),
                   fontsize=6)
-        ct+=1
+        ct += 1
+
 
 BATCH_SIZE = 200
 if __name__ == "__main__":
     training_set = CoRDataset("data/training/")
-    training_loader = DataLoader(training_set, shuffle=True)
+    training_loader = DataLoader(training_set, batch_size=BATCH_SIZE, shuffle=True)
     validation_set = CoRDataset("data/validation/")
     validation_loader = DataLoader(validation_set, shuffle=False)
     test_set = CoRDataset("data/test/")
     test_loader = DataLoader(test_set, shuffle=False)
-
-pokemonmodel = torch.nn.Sequential(torch.nn.Linear(56*56,20),
+    pokemonmodel = torch.nn.Sequential(torch.nn.Linear(56 * 56, 1024),
                                        torch.nn.ReLU(),
-                                       torch.nn.Linear(20,10),
+                                       torch.nn.Linear(1024, 512),
                                        torch.nn.ReLU(),
-                                       torch.nn.Linear(10,1),
+                                       torch.nn.Linear(512, len(training_set.class_map)),
                                        )
-criterion = torch.nn.CrossEntropyLoss()
-#for val in [0, 10**(-5), 10**(-2), 10**0, 10**2]:
-optimizer = torch.optim.SGD(pokemonmodel.parameters(), lr = 0.001, momentum=0.9, weight_decay = 10**(-5))
-Learning(100, pokemonmodel, criterion, optimizer, BATCH_SIZE, training_loader, validation_loader)
-Testmodel("best_model.pth", criterion, test_loader)
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(pokemonmodel.parameters(), lr=0.001, weight_decay=1e-4)
+    Learning(100, pokemonmodel, criterion, optimizer, BATCH_SIZE, training_loader, validation_loader)
+    Testmodel("best_model.pth", criterion, test_loader)
